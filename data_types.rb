e6ee9c277f1a5d966b1f985c5145f666dc2a4067
IvanR96 <irios96@hotmail.com>
2024-03-06 21:39:22 -0800

    arithmetic rules

---
+++ data_types.rb
@@ -0,0 +1,13 @@
+#Modulus 
+
+8 % 2 #=> 0 (8 / 2 = 4; no remainder)
+
+10 % 2 #=> (10 / 4 = 2 with a remainder of 2)
+
+# Arithmetics in RUBY answers always INTEGER
+
+17 / 5 #=> 3, not 3.4
+
+17 / 5.0 #=> 3.4
+
+
