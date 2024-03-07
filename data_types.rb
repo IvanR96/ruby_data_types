0e515e20fcde4bd539aa1245fdc98d296fabc6fe
IvanR96 <irios96@hotmail.com>
2024-03-06 23:16:25 -0800

    more methods added

--- data_types.rb
+++ data_types.rb
@@ -36,3 +36,53 @@
 
 #concatenation with concat method
 "Welcome ".concat("to ").concat("Odin!") #=> "Welcome to Odin!"
+
+#access to substrings
+"hello"[0]      #=> "h"
+
+"hello"[0..1]   #=> "he"
+
+"hello"[0, 4]   #=> "hell"
+
+"hello"[-1]     #=> "o"
+
+#Escape characters
+\\  #=> Need a backslash in your string?
+\b  #=> Backspace
+\r  #=> Carriage return, for those of you that love typewriters
+\n  #=> Newline. You'll likely use this one the most.
+\s  #=> Space
+\t  #=> Tab
+\"  #=> Double quotation mark
+\'  #=> Single quotation mark
+
+#Interpolation
+name = "Odin"
+
+puts "Hello, #{name}" #=> "Hello, Odin"
+
+puts 'Hello, #{name}' #=> "Hello, #{name}"
+
+
+#Common string methods
+"hello".capitalize #=> "Hello"
+
+"hello".include?("lo")  #=> true
+
+"hello".include?("z")   #=> false
+
+"hello".upcase  #=> "HELLO"
+
+"Hello".downcase  #=> "hello"
+
+"hello".empty?  #=> false
+
+"".empty?       #=> true
+
+"hello".length  #=> 5
+
+"hello".reverse  #=> "olleh"
+
+"hello world".split  #=> ["hello", "world"]
+
+"hello".split("")    #=> ["h", "e", "l", "l", "o"]
